77e91a7ba13c38044e98137842c7b9ff12f97270
Ken Warren <134076996+ken-warren@users.noreply.github.com>
2024-06-02 10:58:04 +0300

    Update data cleaning.sql

--- assets/sql/data cleaning.sql
+++ assets/sql/data cleaning.sql
@@ -26,7 +26,8 @@ CREATE VIEW view_uk_youtubers_2024 AS
 
 SELECT
 
-	CAST(SUBSTRING(NOMBRE, 1, CHARINDEX ('@', NOMBRE) -1) AS VARCHAR(100))as channel_name,
+	CAST(SUBSTRING(NOMBRE, 1, CHARINDEX ('@', NOMBRE) -1) AS VARCHAR(100)) AS
+		channel_name,
 		total_subscribers,
 		total_views,
 		total_videos
